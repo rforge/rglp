6cd6faba05cf2c6cd7e8127dcc439640c1698450
stefan7th <stefan7th@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2007-12-16 19:37:44 +0000

    implementation of column bounds improved; changed function name to Rglpk_solve -> Package now named Rglpk
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/rglp@6 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/glp_solve.R
+++ pkg/R/glp_solve.R
@@ -1,8 +1,8 @@
 ## the R-ported GNU Linear Programming kit
 ## solve function --- C Interface
 
-glp_solve <- function(obj, mat, dir, rhs, int = NULL, max = FALSE,
-                      bounds = list(lower=list(NULL,NULL),upper=list(NULL,NULL)),
+Rglpk_solve <- function(obj, mat, dir, rhs, int = NULL, max = FALSE,
+                      bounds = NULL,
                       verbose = FALSE){
   
   ## direction of optimization
@@ -31,13 +31,18 @@ glp_solve <- function(obj, mat, dir, rhs, int = NULL, max = FALSE,
   ## do we have a mixed integer linear program?
   is_integer <- any(integers)
 
+  if(is.null(bounds))
+    bounds <- list()
+  #list(lower=list(NULL,NULL),upper=list(NULL,NULL))
   bounds <- glp_bounds(bounds, n_of_objective_vars)
+
+
   ## call the C interface - this actually runs the solver
   x <- glp_call_interface(obj, n_of_objective_vars, constraint_matrix$i,
                           constraint_matrix$j, constraint_matrix$v, length(constraint_matrix$v),
                           rhs, direction_of_constraints, n_of_constraints, is_integer,
-                          integers, direction_of_optimization, bounds$lower[[1L]],
-                          bounds$lower[[2L]], bounds$upper[[1L]], bounds$upper[[2L]], verb)
+                          integers, direction_of_optimization, bounds[,1],
+                          bounds[,2], bounds[,3], verb)
   out <- list(optimum=NULL, solution=NULL)
   out$optimum <- x$lp_optimum
   out$solution <- x$lp_objective_vars_values
@@ -89,34 +94,74 @@ glp_integers <- function(x, n){
   out
 }
 
+## fixes the GLPK bound types given a data.frame with bounds
+## GLP_FR 1 free variable
+## GLP_LO 2 variable with lower bound
+## GLP_UP 3 variable with upper bound
+## GLP_DB 4 double-bounded variable
+## GLP_FX 5 fixed variable
+glp_fix_bound_type <- function(x){
+  if(!inherits(x,"bound_table"))
+    stop("'x' is not of class 'bound_table'")
+  x$type <- ifelse(is.finite(x$lower),
+                   ifelse(is.finite(x$upper), 4L, 3L),
+                   ifelse(is.finite(x$upper), 2L, 1L))
+  x$type[x$upper==x$lower] <- 5L
+  x
+}
+
+## TODO: should be a generic function providing methods for
+## different representations (e.g., a matrix, list of vectors, ...)
+##                   
 glp_bounds <- function(x, n){
   ## Input validation
   if(!is.list(x))
     stop("Bounds have to be of type list")
-  if((length(x)!=2) || (is.null(x$upper)) || (is.null(x$lower)))
-    stop("Bounds have to be of type list with 2 list elements called upper and lower")
-#  if(!is.integer(x$lower[[1]]) || !is.integer(x$upper[[1]]))
-#    stop("Bound indices have to be of type integer")
-#  if((max(x$lower[[1]]) > n) || (max(x$upper[[1]]) > n))
-#    stop("Bound indices must not exceed number of objective variables")
-  if((length(x$lower[[1]]) != length(x$lower[[2]])) || (length(x$upper[[1]]) != length(x$upper[[2]])))
-    stop("Length of bound indices must be equal to the length of the corresponding bound values")
-  if(any(duplicated(x$lower[[1]])) || any(duplicated(x$upper[[1]])))
-    stop("Duplicated entries in bound indices")
-  if(any(x$lower[[1]]==Inf) || any(x$upper[[1]]==-Inf))
-    stop("Lower bound cannot be 'Inf'; upper bound cannot be '-Inf'")
-  ## reorder bounds, bound indices must be ascending
-  if(!is.null(x$lower[[1]])){
-    ord <- order(x$lower[[1]])
-    x$lower[[1]] <- x$lower[[1]][ord]
-    x$lower[[2]] <- x$lower[[2]][ord]
+
+  ## Initialize default matrix
+  bound_table <- expand.grid(type=rep.int(2L,n), upper=0.0, lower=Inf)
+  class(bound_table) <- c("bound_table", class(bound_table))
+  
+  ## Lower bounds
+  lower <- x$lower
+  if(!is.null(lower)){
+    if(!is.integer(lower[[1]]))
+      stop("Bound indices have to be of type integer")
+    if(length(lower[[1]]) != length(lower[[2]]))
+      stop("Length of bound indices must be equal to the length of the corresponding bound values")
+    if(any(lower[[1]]==Inf))
+      stop("Lower bound cannot be 'Inf'")
+    if(any(duplicated(lower[[1]])))
+      stop("Duplicated entries in bound indices")
+    if((max(lower[[1]]) > n))
+      stop("Bound indices must not exceed number of objective variables") 
+    ## if everything is OK set new lower bounds
+    bound_table[lower[[1]],2] <- lower[[2]]
   }
-  if(!is.null(x$upper[[1]])){
-    ord <- order(x$upper[[1]])
-    x$upper[[1]] <- x$upper[[1]][ord]
-    x$upper[[2]] <- x$upper[[2]][ord]
-  } 
-  x
+
+  ## Upper bounds
+  upper <- x$upper
+  if(!is.null(upper)){
+    if(!is.integer(upper[[1]]))
+      stop("Bound indices have to be of type integer")
+    if(length(upper[[1]]) != length(upper[[2]]))
+      stop("Length of bound indices must be equal to the length of the corresponding bound values")
+    if(any(upper[[1]]==-Inf))
+      stop("Upper bound cannot be '-Inf'")
+    if(any(duplicated(upper[[1]])))
+      stop("Duplicated entries in bound indices")
+    if((max(upper[[1]]) > n))
+      stop("Bound indices must not exceed number of objective variables")
+    ## so far, the same as with lower bounds but in addition we have to be
+    ## sure that upper bounds are greater than or equal to lower bounds
+    if(any(bound_table[upper[[1]],2] > upper[[2]]))
+      stop("Upper bounds have to be greater than or equal to lower bounds")
+    bound_table[upper[[1]],3] <- upper[[2]]
+  }
+
+  ## Fix bound types
+  out <- glp_fix_bound_type(bound_table)
+  out
 }
   
 
@@ -125,7 +170,7 @@ glp_call_interface <- function(lp_objective_coefficients, lp_n_of_objective_vars
                                lp_n_of_values_in_constraint_matrix, lp_right_hand_side,
                                lp_direction_of_constraints, lp_n_of_constraints, lp_is_integer,
                                lp_objective_var_is_integer, lp_direction_of_optimization,
-                               lp_lower_bounds_i, lp_lower_bounds_v, lp_upper_bounds_i, lp_upper_bounds_v,
+                               lp_bounds_type, lp_bounds_lower, lp_bounds_upper,
                                verbose){
   out <- .C("R_glp_solve",
             lp_direction_of_optimization= as.integer(lp_direction_of_optimization),
@@ -140,16 +185,15 @@ glp_call_interface <- function(lp_objective_coefficients, lp_n_of_objective_vars
             lp_constraint_matrix_i      = as.integer(lp_constraint_matrix_i),
             lp_constraint_matrix_j      = as.integer(lp_constraint_matrix_j),
             lp_constraint_matrix_values = as.double(lp_constraint_matrix_v),
-            lp_lower_bounds_i           = as.integer(lp_lower_bounds_i),
-            lp_lower_bounds_v           = as.double(lp_lower_bounds_v),
-            lp_n_of_bounds_l            = as.integer(length(lp_lower_bounds_i)),
-            lp_upper_bounds_i           = as.integer(lp_upper_bounds_i), 
-            lp_upper_bounds_v           = as.double(lp_upper_bounds_v),
-            lp_n_of_bounds_u            = as.integer(length(lp_upper_bounds_i)),
+            lp_bounds_type              = as.integer(lp_bounds_type),
+            lp_bounds_lower             = as.double(lp_bounds_lower),
+            ## lp_n_of_bounds_l            = as.integer(length(lp_lower_bounds_i)),
+            lp_bounds_upper             = as.double(lp_bounds_upper), 
+            ## lp_n_of_bounds_u            = as.integer(length(lp_upper_bounds_i)),
             lp_optimum                  = double(1),
             lp_objective_vars_values    = double(lp_n_of_objective_vars),
             lp_verbosity                = as.integer(verbose),
             lp_status                   = integer(1),
-            NAOK = TRUE, PACKAGE = "Rglp")
+            NAOK = TRUE, PACKAGE = "Rglpk")
   out
 }
